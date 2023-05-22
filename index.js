b352aa416285754df305b67531a04314111c96f0
Huda <xhudan@yandex.com>
2023-05-22 17:08:26 +0700

    edit port 4000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { product } = require('./models');
 const productRoutes = require('./routes/');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 app.use(bodyParser.json());
